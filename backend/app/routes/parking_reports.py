c61cc5ef99f37394e2263cf06c495e0b2255ca8e
juanromanRP13 <juanroman1313@gmail.com>
2026-04-09 18:56:51 +0200

    GET zone stats

--- backend/app/routes/parking_reports.py
+++ backend/app/routes/parking_reports.py
@@ -8,7 +8,7 @@ from app.security import get_current_user
 router = APIRouter(prefix="/reports", tags=["Reports"])
 
 @router.post("/createReport")
-def createReport(
+def create_report(
     report: ParkingReportCreate, 
     current_user: User = Depends(get_current_user)
     ):
@@ -25,7 +25,23 @@ def createReport(
     return new_report
 
 @router.get("/my-reports", response_model=list[ParkingReportResponse])
-def getUserReports(current_user: User = Depends(get_current_user)):
+def get_user_reports(current_user: User = Depends(get_current_user)):
     with SessionLocal() as session:
         user_reports = session.query(ParkingReport).filter(ParkingReport.user_id == current_user.id).all()
         return user_reports
+    
+@router.get("/stats/{zone_id}")
+def get_stats(zone_id:int,current_user: User = Depends(get_current_user)):
+    with SessionLocal() as session:
+        total_reports = session.query(ParkingReport).filter(ParkingReport.zone_id == zone_id).count()
+        available_reports = session.query(ParkingReport).filter(ParkingReport.zone_id == zone_id, ParkingReport.availability == True).count()
+    if total_reports == 0:
+        occupancy_rate=0
+    else:
+        occupancy_rate = available_reports / total_reports
+    return {
+        "zone_id": zone_id,
+        "total_reports": total_reports,
+        "available_reports": available_reports,
+        "occupancy_rate": occupancy_rate
+    }
