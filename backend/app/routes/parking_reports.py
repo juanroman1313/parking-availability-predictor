03452df5b5354f9fae0858158108fd8a98c2a9b3
juanromanRP13 <juanroman1313@gmail.com>
2026-04-22 21:07:07 +0200

    Fix in create report with a unexisting zone

--- backend/app/routes/parking_reports.py
+++ backend/app/routes/parking_reports.py
@@ -3,6 +3,7 @@ from app.base import SessionLocal
 from app.models.user import User
 from app.models.parking_report import ParkingReport
 from app.models.parking_availability_stats import ParkingAvailabilityStats
+from app.models.zone import Zone
 from app.schemas.parking_report_schema import ParkingReportCreate, ParkingReportResponse
 from app.security import get_current_user
 from app.stats_utils import calculate_probability
@@ -15,14 +16,17 @@ def create_report(
     report: ParkingReportCreate, 
     current_user: User = Depends(get_current_user)
     ):
-    dtTrunct = report.time_slot.replace(second=0,minute=0)
-    new_report = ParkingReport(
-        user_id=current_user.id,
-        zone_id=report.zone_id,
-        time_slot=dtTrunct,
-        availability=report.availability
-    )
     with SessionLocal() as session:
+        existing_zone = session.query(Zone).filter(Zone.id == report.zone_id).first()
+        if not existing_zone:
+            raise HTTPException(status_code=400, detail="Zone does not exist")
+        dtTrunct = report.time_slot.replace(second=0,minute=0)
+        new_report = ParkingReport(
+            user_id=current_user.id,
+            zone_id=report.zone_id,
+            time_slot=dtTrunct,
+            availability=report.availability
+        )
         session.add(new_report)
         session.commit()
         #Consultar si ya existe en stats una fila con zona y time slot
